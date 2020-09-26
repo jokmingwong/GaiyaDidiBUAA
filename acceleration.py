c8f604568d8ec708d2f2aef7e00e69c85a9296ee
khunkin <657828489@qq.com>
2020-09-26 23:54:56 +0800

    跑通 acceleration 和 road, 但是acceleration对于加速度的计算方式存疑

--- acceleration.py
+++ acceleration.py
@@ -7,7 +7,7 @@ import sys
 import math
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
-
+import time as TIME
 # 滴滴GPS数据
 
 x_axis_offset = 309741 - 310162 - 8
@@ -32,12 +32,14 @@ try:
     with connection.cursor() as cursor:
         # Create a new record
         # sql = f"SELECT * FROM gps_1001 order by cur_time"
-        sql = f"SELECT * FROM gps_1001 order by cur_time limit 500000"
+        print("时间：", TIME.asctime(TIME.localtime(TIME.time())), " 建立连接")
+        sql = f"SELECT * FROM gps_1001 order by cur_time limit 10000"
         cursor.execute(sql)
         result = cursor.fetchall()
     connection.commit()
+    print("时间：", TIME.asctime(TIME.localtime(TIME.time())), " 拉取数据完成")
+    print("时间：", TIME.asctime(TIME.localtime(TIME.time())), " 开始格式化order")
     for line in result:
-        line_id = line['id']
         driver_id = line['driver_id']
         order_id = line['order_id']
         cur_time = int(line['cur_time'])
@@ -46,7 +48,7 @@ try:
         if order_id not in orders:
             orders[order_id] = []
         x, y, _, __ = utm.from_latlon(lat, lng)
-        orders[order_id].append((cur_time,x + x_axis_offset,y + y_axis_offset,line_id))
+        orders[order_id].append((cur_time,x + x_axis_offset,y + y_axis_offset))
         # orders[order_id][0][cur_time] = x + x_axis_offset
         # orders[order_id][1][cur_time] = y + y_axis_offset
         # orders[order_id][2].append(cur_time)
@@ -54,7 +56,7 @@ try:
 finally:
     connection.close()      
 
-
+print("数据格式化成功，结束连接")
 
 
 NODE_MAX_DIS2=10000
@@ -67,23 +69,23 @@ def distance2(node1,node2):
 
 order_cnt = 0
 order_total = len(orders)
-speeds = {}
 flag = True
 
 
 accs = []
 
 for order in orders:
+    print("时间：", TIME.asctime(TIME.localtime(TIME.time())), " 处理订单：", order)
     orders[order] = sorted(orders[order],key=compare_by_time)
     order_list = orders[order]
 
     time = [t[0] for t in orders[order]]
     xs = [t[1] for t in orders[order]]
     ys = [t[2] for t in orders[order]]
-    ids = [t[3] for t in orders[order]]
+    ids = [t[0] for t in orders[order]]
 
     if order_cnt%100 ==0:
-        print(str(order_cnt)+'/'+str(order_total))
+        print("时间：", TIME.asctime(TIME.localtime(TIME.time())), " 进度：", str(order_cnt)+'/'+str(order_total))
     order_cnt+=1
 
     illegal = False
@@ -138,6 +140,7 @@ for order in orders:
         i = len(gaus_xs) - 1
         velocity.append(math.sqrt(distance2((gaus_xs[i-1],gaus_ys[i-1]),(gaus_xs[i],gaus_ys[i])))/(time[i] - time[i-1]))
 
+        speeds = {}
         speeds[ids[0]]=(0,0,0)
         tmp_speed = []
         tmp_speed.append((0,0,0))
